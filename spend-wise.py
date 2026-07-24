ad438ef540136f55baf1962685a9bbbd2188a0c8
Pranav Ashar <pranav.ashar11@gmail.com>
2026-07-24 21:29:05 +0530

    Design main menu interface & Implement application navigation

--- spend-wise.py
+++ spend-wise.py
@@ -2,6 +2,12 @@
 name = input("Enter your name : ")
 print(f"Hello, {name} Welcome to the Expense Tracker !")
 
+# print("""
+#  __________
+# ( |||      )
+#  ‾‾‾‾‾‾‾‾‾‾""")
+
+
 def menu():
     print("""
 1. Add Expense
@@ -21,22 +27,25 @@ def menu():
     if (user_response == 1):
         print("Add Expense")
     elif (user_response == 2):
-        print("View All Expenses Functionality")
+        print("View All Expenses")
     elif (user_response == 3):
-        print("Search Expense Functionality")
+        print("Search Expense")
     elif (user_response == 4):
-        print("Monthly Summary Functionality")
+        print("Monthly Summary")
     elif (user_response == 5):
-        print("Category Analysis Functionality")
+        print("Category Analysis")
     elif (user_response == 6):
-        print("Budget Tracker Functionality")
+        print("Budget Tracker")
     elif (user_response == 7):
-        print("Savings Report Functionality")
+        print("Savings Report")
     elif (user_response == 8):
-        print("Edit Expense Functionality")
+        print("Edit Expense")
     elif (user_response == 9):
-        print("Delete Expense Functionality")
+        print("Delete Expense")
     elif (user_response == 10):
         print("Exiting the Expense Tracker. Goodbye!")
     else:
         print("Invalid Response")
+
+while True:
+    menu()
